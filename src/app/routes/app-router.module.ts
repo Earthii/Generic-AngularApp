08a0e1b9b221e7243f2c68d06fb8cdcb19c5bc4c
Earthii <xiao_eric@hotmail.com>
2018-10-07 15:37:05 -0400

    Basic login component

--- src/app/routes/app-router.module.ts
+++ src/app/routes/app-router.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from './../shared.module';
 
 import { HomeComponent } from './../home/home.component';
+import { LoginComponent } from './../login/login.component';
 import { PageNotFoundComponent } from '../pageNotFound/pageNotFound.component';
 
 const appRoutes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
   { path: '404', name: 'Not Found', component: PageNotFoundComponent },
   { path: '**', redirectTo: '404' }
 ];
@@ -18,4 +20,8 @@ const appRoutes = [
 })
 export class AppRouterModule {}
 
-export const routingComponents = [HomeComponent, PageNotFoundComponent];
+export const routingComponents = [
+  HomeComponent,
+  LoginComponent,
+  PageNotFoundComponent
+];
